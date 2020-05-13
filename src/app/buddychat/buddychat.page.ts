8a9a3e81a9918cfb03e47976967849f9e2a00cf0
Phú Gia <weed81120@gmail.com>
2020-05-13 21:22:03 +0700

    fontend

--- src/app/buddychat/buddychat.page.ts
+++ src/app/buddychat/buddychat.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, NgZone, ViewChild } from '@angular/core';
-import { NavController,Events, AlertController, IonContent, LoadingController } from '@ionic/angular';
+import { NavController, Events, AlertController, IonContent, LoadingController } from '@ionic/angular';
 import { RequestsService } from '../services/requests/requests.service'
 import { ChatService } from '../services/chat/chat.service';
 import { ImghandlerService } from '../services/imghandler/imghandler.service';
@@ -11,7 +11,7 @@ import * as firebase from 'firebase';
   styleUrls: ['./buddychat.page.scss'],
 })
 export class BuddychatPage implements OnInit {
-  @ViewChild('content' , {static: false}) content: IonContent;
+  @ViewChild('content', { static: false }) content: IonContent;
   newmessage;
   buddy: any;
   allmessages = [];
@@ -26,13 +26,13 @@ export class BuddychatPage implements OnInit {
     public zone: NgZone,
     public loadingCtrl: LoadingController,
     public imgstore: ImghandlerService
-  ) { 
+  ) {
     this.buddy = this.chatservice.buddy;
     this.photoURL = firebase.auth().currentUser.photoURL;
     this.scrollto();
-    this.events.subscribe('newmessage' , () => {
+    this.events.subscribe('newmessage', () => {
       this.allmessages = [];
-      this.zone.run( () => {
+      this.zone.run(() => {
         this.allmessages = this.chatservice.buddymessages;
         /* for( var key in this.allmessages) {
           if(this.allmessages[key].message.substring(0 , 4) == 'http')
@@ -44,7 +44,7 @@ export class BuddychatPage implements OnInit {
     })
   }
   addmessage() {
-    this.chatservice.addnewmessage(this.newmessage).then( () => {
+    this.chatservice.addnewmessage(this.newmessage).then(() => {
       this.content.scrollToBottom();
       this.newmessage = '';
     })
@@ -55,30 +55,30 @@ export class BuddychatPage implements OnInit {
   }
 
   scrollto() {
-    setTimeout( () => {
+    setTimeout(() => {
       this.content.scrollToBottom();
     }, 1000);
   }
 
-   async sendPicMsg() {
+  async sendPicMsg() {
     let loader = await this.loadingCtrl.create({
       message: 'Please wait'
     });
     loader.present();
-    this.imgstore.picmsgstore().then( (imgurl) => {
+    this.imgstore.picmsgstore().then((imgurl) => {
       loader.dismiss();
-      this.chatservice.addnewmessage(imgurl).then( () => {
+      this.chatservice.addnewmessage(imgurl).then(() => {
         this.scrollto();
-        this.newmessage= '';
+        this.newmessage = '';
       })
-    }).catch( err => {
+    }).catch(err => {
       alert(err);
       loader.dismiss();
     })
-  } 
+  }
   ngOnInit() {
-    
-    
+
+
   }
 
 }
--- src/app/groupchat/groupchat.page.ts
+++ src/app/groupchat/groupchat.page.ts
@@ -10,7 +10,7 @@ import * as firebase from 'firebase'
   styleUrls: ['./groupchat.page.scss'],
 })
 export class GroupchatPage implements OnInit {
-  @ViewChild('content' , {static: false}) content: IonContent
+  @ViewChild('content', { static: false }) content: IonContent
   owner: boolean = false;
   groupName;
   newmessage;
@@ -27,22 +27,22 @@ export class GroupchatPage implements OnInit {
     public loadingCtrl: LoadingController,
     public imgstore: ImghandlerService,
     public events: Events
-  ) { 
+  ) {
     this.groupName = this.route.snapshot.paramMap.get('groupName')
     this.alignuid = firebase.auth().currentUser.uid;
     this.photoURL = firebase.auth().currentUser.photoURL;
-    
-    this.groupservice.getownership(this.groupName).then( res => {
-      if(res) {
+
+    this.groupservice.getownership(this.groupName).then(res => {
+      if (res) {
         this.owner = true;
       }
-    }).catch( err => {
+    }).catch(err => {
       alert(err);
     })
     this.groupservice.getgroupmsgs(this.groupName);
-    this.events.subscribe('newgroupmsg' , () => {
+    this.events.subscribe('newgroupmsg', () => {
       this.allgroupmsgs = [];
-      this.imgornot= [];
+      this.imgornot = [];
       this.allgroupmsgs = this.groupservice.groupmsgs;
       for (var key in this.allgroupmsgs) {
         var d = new Date(this.allgroupmsgs[key].timestamp);
@@ -50,12 +50,12 @@ export class GroupchatPage implements OnInit {
         var minutes = "0" + d.getMinutes();
         var month = d.getMonth();
         var da = d.getDate();
- 
+
         var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
           "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-        
+
         var formattedTime = monthNames[month] + "-" + da + "-" + hours + ":" + minutes.substr(-2);
- 
+
         this.allgroupmsgs[key].timestamp = formattedTime;
         if (this.allgroupmsgs[key].message.substring(0, 4) === 'http') {
           this.imgornot.push(true);
@@ -83,7 +83,7 @@ export class GroupchatPage implements OnInit {
           }
         },
         {
-          text:'Remove member',
+          text: 'Remove member',
           icon: 'remove-circle',
           handler: () => {
             this.navCtrl.navigateForward('/groupmember')
@@ -93,18 +93,18 @@ export class GroupchatPage implements OnInit {
           text: 'Group info',
           icon: 'person',
           handler: () => {
-            this.navCtrl.navigateForward(['groupinfo' , {groupName: this.groupName}])
+            this.navCtrl.navigateForward(['groupinfo', { groupName: this.groupName }])
           }
         },
         {
           text: 'Delete Group',
           icon: 'trash',
           handler: () => {
-            this.groupservice.deletegroup().then( () => {
+            this.groupservice.deletegroup().then(() => {
               this.router.navigateByUrl('/tabs/chats')
             }).catch(err => {
               console.log(err);
-              
+
             })
           }
         },
@@ -122,64 +122,64 @@ export class GroupchatPage implements OnInit {
 
   async presentMemberSheet() {
     let sheet = await this.actionSheetController.create({
-      header: 'Group Actions' ,
+      header: 'Group Actions',
       buttons: [
         {
           text: 'Leave Group',
           icon: 'log-out',
           handler: () => {
-            this.groupservice.leavegroup().then( () => {
+            this.groupservice.leavegroup().then(() => {
               this.router.navigateByUrl('/tabs/chats')
-            }).catch( err => {
+            }).catch(err => {
               console.log(err);
             })
           }
-      },
-      {
-        text: 'Group Info',
-        icon: 'person',
-        handler: () => {
-          this.navCtrl.navigateForward(['groupinfo' , {groupName: this.groupName}])
+        },
+        {
+          text: 'Group Info',
+          icon: 'person',
+          handler: () => {
+            this.navCtrl.navigateForward(['groupinfo', { groupName: this.groupName }])
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          icon: 'backspace',
+          handler: () => {
+            console.log('Cancelled');
+          }
         }
-      },
-      {
-        text: 'Cancel',
-        role: 'cancel',
-        icon: 'backspace',
-        handler: () => {
-          console.log('Cancelled');
-        }
-      }
       ]
     })
     sheet.present();
   }
 
   addgroupmsg() {
-    this.groupservice.addgroupmsg(this.newmessage).then( () => {
-      this.scrollto();
+    this.groupservice.addgroupmsg(this.newmessage).then(() => {
+      this.content.scrollToBottom();
       this.newmessage = '';
     })
   }
 
   scrollto() {
-    setTimeout( () => {
+    setTimeout(() => {
       this.content.scrollToBottom();
     }, 1000);
   }
-  
+
   async sendpicmsg() {
     let loader = await this.loadingCtrl.create({
       message: 'Please wait...'
     });
     loader.present();
-    this.imgstore.picmsgstore().then( imgurl => {
+    this.imgstore.picmsgstore().then(imgurl => {
       loader.dismiss();
-      this.groupservice.addgroupmsg(imgurl).then( () => {
+      this.groupservice.addgroupmsg(imgurl).then(() => {
         this.scrollto();
         this.newmessage = '';
       })
-    }).catch( err => {
+    }).catch(err => {
       alert(err);
       loader.dismiss();
     })
