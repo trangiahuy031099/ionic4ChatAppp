a468049a57b28db031f2d2ebc715352ec7131ee2
Huy Tran <kakahuy113@gmail.com>
2020-06-25 21:45:32 +0700

    add delechat and fix bug of phu ngu

--- src/app/chats/chats.page.ts
+++ src/app/chats/chats.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, NgZone, Input } from "@angular/core";
 import { NavController, Events, AlertController } from "@ionic/angular";
 import { RequestsService } from "../services/requests/requests.service";
 import { ChatService } from "../services/chat/chat.service";
-
+import * as firebase from 'firebase'
 @Component({
   selector: "app-chats",
   templateUrl: "./chats.page.html",
@@ -11,6 +11,7 @@ import { ChatService } from "../services/chat/chat.service";
 export class ChatsPage implements OnInit {
   myfriends;
   presence$;
+  firebuddychats = firebase.database().ref('/buddychats')
 
   constructor(
     public navCtrl: NavController,
@@ -35,7 +36,32 @@ export class ChatsPage implements OnInit {
   ionViewDidLeave() {
     this.events.unsubscribe("friends");
   }
+  async deletechat(id) {
+    const alert = await this.alertCtrl.create({
+      header: 'Confirm!',
+      message: 'Do u want to delete this chat content? <br/> You can recoved it man!!!',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Okay',
+          handler: () => {
+            this.firebuddychats.child(firebase.auth().currentUser.uid).child(id).remove().then( () => {
+              console.log('It done');
+            })
+          }
+        }
+      ]
+    });
 
+    await alert.present();
+    
+  }
   addbuddy() {
     this.navCtrl.navigateForward("/buddies");
   }
